481f57342955e15520790c719f4434b828bc9827
Punit Soni <punit.soni33@gmail.com>
2023-01-26 23:11:53 +0530

    (feature:mapValues): object - mapValues method added
    resolves #47

---
+++ test/object/mapValues.test.ts
@@ -0,0 +1,24 @@
+import _mapValues from 'lodash/mapValues';
+import { mapValues } from '../../src/object';
+
+const users = {
+	'fred': { 'user': 'fred', 'age': 40 },
+	'pebbles': { 'user': 'pebbles', 'age': 1 }
+};
+const predicate = (o: any) => { return o.age; };
+
+describe('Object', () => {
+	describe('mapValues', () => {
+		it("mapValues(users, predicate)", () => {
+			expect(mapValues(users, predicate)).toEqual(
+				_mapValues(users, predicate)
+			);
+		});
+
+		it("mapValues(users, 'age')", () => {
+			expect(mapValues(users, 'age')).toEqual(
+				_mapValues(users, 'age')
+			);
+		});
+	});
+});
