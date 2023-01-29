1598f7a5bb142fd4f51301edd266af0996445c92
Punit Soni <punit.soni33@gmail.com>
2023-01-29 16:23:21 +0530

    linting fixed

--- test/util/_matches.test1.ts
+++ test/util/_matches.test1.ts
@@ -1,26 +1,24 @@
 import _matches from 'lodash/matches';
 import _filter from 'lodash/filter';
 import { matches } from '../../src/util';
-import { filter } from '../../src/collection'
-import { cloneDeep } from '../../src/lang'
+import { filter } from '../../src/collection';
+import { cloneDeep } from '../../src/lang';
 
 describe('Util', () => {
 	describe('matches', () => {
 		it("matches({ 'a': 4, 'c': 6 })", () => {
 			const objects = [
-				{ 'a': 1, 'b': 2, 'c': 3 },
-				{ 'a': 4, 'b': 5, 'c': 6 }
+				{ a: 1, b: 2, c: 3 },
+				{ a: 4, b: 5, c: 6 },
 			];
 
-			expect(filter(objects, matches({ 'a': 4, 'c': 6 }))).toEqual(_filter(objects, _matches({ 'a': 4, 'c': 6 })));
+			expect(filter(objects, matches({ a: 4, c: 6 }))).toEqual(
+				_filter(objects, _matches({ a: 4, c: 6 }))
+			);
 		});
 
-		it("matches - sources", () => {
-			let sources = [
-				{ 'a': { 'b': 2, 'c': 3 } },
-				{ 'a': 1, 'b': 2 },
-				{ 'a': 1 }
-			];
+		it('matches - sources', () => {
+			let sources = [{ a: { b: 2, c: 3 } }, { a: 1, b: 2 }, { a: 1 }];
 
 			sources.forEach(function (source: any, index: number) {
 				let object = cloneDeep(source),
