080d07531ae3a1d861fe4c32c8d6b65eca99433e
Punit Soni <punit.soni33@gmail.com>
2023-02-18 17:35:07 +0530

    zipObjectDeep method code updated

--- src/array/_zipObjectDeep.ts
+++ src/array/_zipObjectDeep.ts
@@ -13,6 +13,23 @@ const zipObjectDeep = (props: any[] = [], values: any[] = []): Object => {
 	}, {});
 
 	return newObject;
+
+	// const result = {};
+
+	// for (let i = 0; i < props.length; i++) {
+	// 	const nestedKeys = props[i].split('.');
+	// 	let currentObject: any = result;
+
+	// 	for (let j = 0; j < nestedKeys.length - 1; j++) {
+	// 		const key = nestedKeys[j];
+	// 		currentObject[key] = currentObject[key] || {};
+	// 		currentObject = currentObject[key];
+	// 	}
+
+	// 	currentObject[nestedKeys[nestedKeys.length - 1]] = values[i];
+	// }
+
+	// return result;
 };
 
 export default zipObjectDeep;
