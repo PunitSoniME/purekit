c3d2a7906b280e620a3f4f8e1ff7192394362ff3
Punit Soni <punit.soni33@gmail.com>
2023-03-11 15:23:26 +0530

    - layout updated

--- docs/pages/[slug].js
+++ docs/pages/[slug].js
@@ -20,7 +20,7 @@ export default function Doc({ meta, content }) {
   return <Layout meta={meta}>{content}</Layout>;
 }
 
-const componentHeader = (title) => `<div id=${title} class="invisible h-0">${title}</div>`;  // `<div id=${title} class="border-l-8 bg-indigo-100 border-l-indigo-500 w-full px-6 mb-6 py-4 rounded text-md md:text-lg font-bold scroll-top">${title}</div>`;
+const componentHeader = (title) => `<div id=${title} class="invisible h-0">${title}</div>`;  // `<div id=${title} class="border-l-8 bg-violet-100 border-l-violet-500 w-full px-6 mb-6 py-4 rounded text-md md:text-lg font-bold scroll-top">${title}</div>`;
 
 export async function getStaticProps() {
   const mainIndexDoc = getDocBySlug('home');
@@ -33,28 +33,32 @@ export async function getStaticProps() {
   for await (let doc of allDocs) {
     const splittedData = doc.split('/');
     const methodDoc = getDocBySlug(splittedData[1], splittedData[0]);
-    // content.push(`<div class="px-2 pb-0 ${i % 2 === 0 ? 'bg-white' : 'bg-slate-100 border-b-2 border-slate-100'}">` + (splittedData[1] !== 'index' ? componentHeader(splittedData[1]) : ' ') + await markdownToHtml(methodDoc.content || '') + '</div>');
 
     if (splittedData[1] !== 'index') {
       content.push(componentHeader(splittedData[1]));
     }
 
+    content.push('<div class="custom-block">');
+
     if (methodDoc.meta.definition) {
-      content.push(`<div class="mx-2 bg-white mb-4">`);
-      content.push(`<div class='border-violet-200 border-l-8 bg-violet-500 border-l-violet-900 text-white tracking-wide font-semibold p-4'>${methodDoc.meta.definition}</div>`);
-      content.push(await markdownToHtml("<small class='pt-4 px-2 md:px-4 font-normal custom-code'>" + methodDoc.meta.description + "</small>" || ''));
+      content.push(`<div class='definition'>${methodDoc.meta.definition}</div>`);
+      content.push(await markdownToHtml("<p class='custom-code'>" + methodDoc.meta.description + "</p>" || ''));
     }
 
     if (splittedData[1] === 'index') {
-      content.push("<div class='not-prose'>");
-      content.push(`<h2 id=${methodDoc.meta.title} class="inline-block font-bold my-8 pb-1 mx-4 rounded-b-md border-b-4 text-2xl md:text-4xl border-violet-600 text-violet-600">`);
-      content.push(await markdownToHtml(methodDoc.meta.description || ''))
+      content.push("<div class='not-prose bg-slate-200 capitalize'>");
+      content.push(`<h2 id=${methodDoc.meta.title} class="inline-block font-bold my-4 md:my-6 pb-1 ml-4 rounded-b-md border-b-4 text-2xl md:text-4xl border-violet-600 text-violet-600">`);
+      content.push(await markdownToHtml(methodDoc.meta.title || ''))
       content.push("</h2>");
       content.push("</div>");
     } else {
+      content.push("<div class='content'>");
       content.push(await markdownToHtml(methodDoc.content || ''));
       content.push("</div>");
     }
+
+    content.push("</div>");
+
 
     ++i;
   }
