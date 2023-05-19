82e888d23dccb82e31d99e4883335b14bd4970f5
Punit Soni <punit.soni33@gmail.com>
2023-05-19 11:08:19 +0530

    bytesize test removed

---
+++ test/lang/_byteSize.test1.ts
@@ -0,0 +1,17 @@
+// import { byteSize } from '../../src/lang';
+
+// describe('Lang', () => {
+// 	describe('byteSize', () => {
+// 		it("byteSize('😀')", () => {
+// 			expect(byteSize('😀')).toEqual(4);
+// 		});
+
+// 		it("byteSize('Hello World')", () => {
+// 			expect(byteSize('Hello World')).toEqual(11);
+// 		});
+
+// 		it('byteSize("Hello, world!")', () => {
+// 			expect(byteSize('Hello, world!')).toEqual(13);
+// 		});
+// 	});
+// });
